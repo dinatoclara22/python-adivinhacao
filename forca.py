54f0578f5180503003d01b4f836018038d7fdd45
= <dinatoclara@gmail.com>
2023-06-07 14:36:51 -0300

    finalizando a lógica do game

--- forca.py
+++ forca.py
@@ -1,31 +1,58 @@
+import random
+
+
 def jogar():
     print("*" * 33)
     print("Bem vindo ao jogo de Forca!")
     print("*" * 33)
 
+    # Opening the file
+    arquivo = open("palavras.txt", "r")
+    palavras = []
 
-palavra_secreta = "banana"
-letras_acertadas = ["_", "_", "_", "_", "_", "_"]
-enforcou = False
-acertou = False
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+    arquivo.close()
 
-print(letras_acertadas)
+    # Choosing a random word
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
 
-while (not enforcou and not acertou):
-    chute = input("Qual letra? ")
-    chute = chute.strip()
+    letras_acertadas = ["_" for letra in palavra_secreta]  # List Comprehension
 
-# Verifying if exists the letter in the secret word
-    index = 0  # position
-    for letra in palavra_secreta:
-        if (chute.upper() == letra.upper()):
-            letras_acertadas[index] = letra
-            print("Encontrei a letra {} na posição {}".format(letra, index))
-        index = index + 1
+    enforcou = False
+    acertou = False
+    erros = 0
 
     print(letras_acertadas)
 
-print("Fim do jogo")
+    while (not enforcou and not acertou):
+
+        chute = input("Qual letra? ")
+        chute = chute.strip().upper()
+
+        if (chute in palavra_secreta):
+            # Verifying if exists the letter in the secret word
+            index = 0  # position
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+    if (acertou):
+        print("Você ganhou!!")
+    else:
+        print("Você perdeu!! :(")
+    print("Fim do jogo")
+
+
 # This snippet is used to run the file directly
 if (__name__ == "__main__"):
     jogar()
